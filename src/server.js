19878d4674afc5ccc0b5185a97745e0ed231542b
shpylovyi1007 <yevheniishpylovyi398@gmail.com>
2024-09-18 13:00:54 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -26,19 +26,6 @@ export const setupServer = () => {
         });
     });
 
-    app.use('*', (req, res, next) => {
-        res.status(404).json({
-            message: 'Not found',
-        });
-    });
-
-    app.use((err, req, res, next) => {
-        res.status(500).json({
-            message: 'Something went wrong',
-            error: err.message,
-        });
-    });
-
     app.get('/contacts', async (req, res) => {
         const contacts = await getAllContact();
 
@@ -65,6 +52,19 @@ export const setupServer = () => {
             status: 200,
             message: `Successfully found contact with id ${contactId}!`,
             data: contact,
+        });
+    });
+
+    app.use('*', (req, res, next) => {
+        res.status(404).json({
+            message: 'Not found',
+        });
+    });
+
+    app.use((err, req, res, next) => {
+        res.status(500).json({
+            message: 'Something went wrong',
+            error: err.message,
         });
     });
 
