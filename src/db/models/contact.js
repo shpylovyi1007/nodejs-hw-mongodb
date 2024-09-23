d5016b5df538d324f2f861660f8dc7309d17cde9
shpylovyi1007 <yevheniishpylovyi398@gmail.com>
2024-09-23 11:09:08 +0300

    3.2

--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -27,7 +27,7 @@ const contactsSchema = new Schema(
     {
         timestamps: true
     },
-    { versionKey: false }
-)
+    { versionKey: false },
+);
 
 export const contactsCollection = model('contacts', contactsSchema);
